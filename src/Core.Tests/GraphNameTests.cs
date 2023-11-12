24297ece1f57fdc62524cd72e53252124b9206d2
hotovecj <test@example.com>
2023-11-12 12:25:06 +0100

    feat: add GraphName

---
+++ src/Core.Tests/GraphNameTests.cs
@@ -0,0 +1,62 @@
+﻿using Core.Exceptions;
+
+namespace Core.Tests;
+
+public class GraphNameTests
+{
+    [Theory]
+    [MemberData(nameof(GetInvalidInputData))]
+    public void Ctor_InvalidInput_ThrowsModelException(string input)
+    {
+        var actual = () =>
+        {
+            _ = new GraphName(input);
+        };
+
+        actual.Should().Throw<ModelException>();
+    }
+
+    [Theory]
+    [InlineData("Name1", "name1", true)]
+    [InlineData("Name1", "name2", false)]
+    public void OperatorEquals_CompareNames_ReturnsSuccess(string leftValue, string rightValue, bool expected)
+    {
+        var leftName = new GraphName(leftValue);
+        var rightName = new GraphName(rightValue);
+
+        var actual = leftName == rightName;
+
+        actual.Should().Be(expected);
+    }
+
+    [Fact]
+    public void GetHashCode_NoInput_CreatesHash()
+    {
+        var graphName = new GraphName("Name");
+
+        var actual = () =>
+        {
+            _ = graphName.GetHashCode();
+        };
+
+        actual.Should().NotThrow();
+    }
+
+    [Fact]
+    public void GetHashCode_SameValues_CreatesSameHash()
+    {
+        const string name = "Name";
+        var graphName1 = new GraphName(name.ToLower());
+        var graphName2 = new GraphName(name.ToUpper());
+
+        graphName1.GetHashCode().Should().Be(graphName2.GetHashCode());
+    }
+
+    public static IEnumerable<object[]> GetInvalidInputData() =>
+        new[]
+        {
+            new object[] { null! },
+            new object[] { string.Empty },
+            new object[] { new string('A', GraphName.NameMaxLength + 1) }
+        };
+}
---
+++ src/Core/GraphName.cs
@@ -0,0 +1,77 @@
+﻿using Core.Exceptions;
+
+namespace Core;
+
+/// <summary>
+/// Object represents graph name
+/// </summary>
+public sealed class GraphName : IEquatable<GraphName>
+{
+    /// <summary>
+    /// Max. limit for name length
+    /// </summary>
+    public const int NameMaxLength = 255;
+
+    /// <summary>
+    /// Parametrized ctor
+    /// </summary>
+    /// <param name="value">Graph name</param>
+    /// <exception cref="ModelException">Custom exception for model validation</exception>
+    public GraphName(string value)
+    {
+        if (string.IsNullOrWhiteSpace(value))
+        {
+            throw new ModelException($"Graph name cannot be empty");
+        }
+
+        if (value.Length > NameMaxLength)
+        {
+            throw new ModelException($"Graph name length must be equal or less than {NameMaxLength} chars");
+        }
+
+        Value = value;
+    }
+
+    /// <summary>
+    /// Value of the name
+    /// </summary>
+    public string Value { get; }
+
+    /// <inheritdoc />
+    public override string ToString() => Value;
+
+    public static implicit operator string(GraphName vertex)
+    {
+        return vertex.Value;
+    }
+
+    /// <inheritdoc />
+    public bool Equals(GraphName? other)
+    {
+        if (ReferenceEquals(null, other)) return false;
+        if (ReferenceEquals(this, other)) return true;
+        return string.Equals(Value, other.Value, StringComparison.OrdinalIgnoreCase);
+    }
+
+    /// <inheritdoc />
+    public override bool Equals(object? obj)
+    {
+        return ReferenceEquals(this, obj) || obj is GraphName other && Equals(other);
+    }
+
+    /// <inheritdoc />
+    public override int GetHashCode()
+    {
+        return StringComparer.OrdinalIgnoreCase.GetHashCode(Value);
+    }
+
+    public static bool operator ==(GraphName? left, GraphName? right)
+    {
+        return Equals(left, right);
+    }
+
+    public static bool operator !=(GraphName? left, GraphName? right)
+    {
+        return !Equals(left, right);
+    }
+}
