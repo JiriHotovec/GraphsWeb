e7ddfe1ff53258a8cc39cc35cc476da9c66785b3
hotovecj <test@example.com>
2023-11-12 12:19:48 +0100

    feat: add WeightedEdge

---
+++ src/Core.Tests/WeightedEdgeTests.cs
@@ -0,0 +1,67 @@
+﻿namespace Core.Tests;
+
+public class WeightedEdgeTests
+{
+    [Theory]
+    [MemberData(nameof(GetWeightInputData))]
+    public void Ctor_WeightInput_ReturnsSuccess(Weight leftWeight, Weight rightWeight, bool expected)
+    {
+        const string srcVertexName = "Name1";
+        const string dstVertexName = "Name2";
+        var left = new WeightedEdge(new Vertex(srcVertexName), new Vertex(dstVertexName), leftWeight);
+        var right = new WeightedEdge(new Vertex(srcVertexName), new Vertex(dstVertexName), rightWeight);
+
+        var actual = left.Weight == right.Weight;
+
+        actual.Should().Be(expected);
+    }
+
+    [Theory]
+    [MemberData(nameof(GetEqualityData))]
+    public void OperatorEquals_CompareEdges_ReturnsSuccess(WeightedEdge leftValue, WeightedEdge rightValue, bool expected)
+    {
+        var actual = leftValue == rightValue;
+
+        actual.Should().Be(expected);
+    }
+
+    public static IEnumerable<object[]> GetWeightInputData() =>
+        new[]
+        {
+            new object[]
+            {
+                new Weight(1),
+                new Weight(1),
+                true
+            },
+            new object[]
+            {
+                new Weight(1),
+                new Weight(2),
+                false
+            }
+        };
+
+    public static IEnumerable<object[]> GetEqualityData() =>
+        new[]
+        {
+            new object[]
+            {
+                new WeightedEdge(new Vertex("Name1"), new Vertex("Name2"), new Weight(1)),
+                new WeightedEdge(new Vertex("Name2"), new Vertex("Name1"), new Weight(1)),
+                true
+            },
+            new object[]
+            {
+                new WeightedEdge(new Vertex("Name1"), new Vertex("Name2"), new Weight(1)),
+                new WeightedEdge(new Vertex("Name1"), new Vertex("Name2"), new Weight(2)),
+                true
+            },
+            new object[]
+            {
+                new WeightedEdge(new Vertex("Name1"), new Vertex("Name2"), new Weight(1)),
+                new WeightedEdge(new Vertex("Name1"), new Vertex("Name3"), new Weight(1)),
+                false
+            }
+        };
+}
---
+++ src/Core/WeightedEdge.cs
@@ -0,0 +1,34 @@
+﻿using Core.Abstractions;
+
+namespace Core;
+
+/// <summary>
+/// Weighted edge (A, B) = (B, A)
+/// </summary>
+public sealed class WeightedEdge : Edge, IWeightedEdge
+{
+    /// <summary>
+    /// Parametrized ctor
+    /// </summary>
+    /// <param name="source">Vertex source</param>
+    /// <param name="destination">Vertex destination</param>
+    /// <param name="weight">Edge's weight</param>
+    /// <exception cref="ArgumentNullException">Parameter cannot be null</exception>
+    public WeightedEdge(Vertex source, Vertex destination, Weight weight)
+        : base(source, destination)
+    {
+        Weight = weight ?? throw new ArgumentNullException(nameof(weight));
+    }
+
+    /// <summary>
+    /// Edge weight
+    /// </summary>
+    public Weight Weight { get; }
+
+    /// <inheritdoc />
+    public override string ToString() => $"({Source}, {Destination}) {Weight}";
+
+    /// <inheritdoc />
+    public override IEdge SwitchVertices() =>
+        new WeightedEdge(Destination, Source, Weight);
+}
