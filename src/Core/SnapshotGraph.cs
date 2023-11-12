77c3212971457a6e985cfa8a01a8595e9570c0a8
hotovecj <test@example.com>
2023-11-12 12:28:08 +0100

    feat: add SnapshotGraph

---
+++ src/Core/SnapshotGraph.cs
@@ -0,0 +1,37 @@
+﻿using Core.Abstractions;
+
+namespace Core;
+
+/// <summary>
+/// Snapshot of specific graph
+/// </summary>
+/// <typeparam name="T">Generic type of IEdge interface</typeparam>
+public sealed class SnapshotGraph<T> where T : IEdge
+{
+    /// <summary>
+    /// Parametrized ctor
+    /// </summary>
+    /// <param name="name">Graph name</param>
+    /// <param name="edges">Graph edges</param>
+    /// <exception cref="ArgumentNullException">Parameter cannot be null</exception>
+    public SnapshotGraph(string name, IEnumerable<T> edges)
+    {
+        if (string.IsNullOrWhiteSpace(name))
+        {
+            throw new ArgumentNullException(nameof(name));
+        }
+
+        Name = name;
+        Edges = edges ?? throw new ArgumentNullException(nameof(edges));
+    }
+
+    /// <summary>
+    /// Graph name
+    /// </summary>
+    public string Name { get; }
+
+    /// <summary>
+    /// Graph edges
+    /// </summary>
+    public IEnumerable<T> Edges { get; }
+}
