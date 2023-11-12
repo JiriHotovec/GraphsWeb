5052ca1964f58b4d86621206c3a604c366ea394b
hotovecj <test@example.com>
2023-11-12 12:44:41 +0100

    feat: add interface IPathResult

---
+++ src/Dijkstra/Abstractions/IPathResult.cs
@@ -0,0 +1,13 @@
+﻿using Core.Abstractions;
+
+namespace Dijkstra.Abstractions;
+
+/// <summary>
+/// An interface for Dijkstra's provider returned path result
+/// </summary>
+public interface IPathResult
+{
+    bool IsSuccess { get; }
+
+    IEnumerable<IWeightedEdge> Paths { get; }
+}
