6cd616d84d96597b1c35475a2bd7a8587836b258
hotovecj <test@example.com>
2023-11-12 12:16:05 +0100

    feat: add interface IWeightedEdge

---
+++ src/Core/Abstractions/IWeightedEdge.cs
@@ -0,0 +1,12 @@
+﻿namespace Core.Abstractions;
+
+/// <summary>
+/// An interface represents weighted edge
+/// </summary>
+public interface IWeightedEdge : IEdge
+{
+    /// <summary>
+    /// Weight of the edge
+    /// </summary>
+    Weight Weight { get; }
+}
