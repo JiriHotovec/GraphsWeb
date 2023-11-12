f487445358ec5325014dfa06b74794764816620d
hotovecj <test@example.com>
2023-11-12 11:49:32 +0100

    feat: add Vertex

---
+++ src/Core.Tests/VertexTests.cs
@@ -0,0 +1,57 @@
+﻿using Core.Exceptions;
+using FluentAssertions;
+
+namespace Core.Tests;
+
+public class VertexTests
+{
+    [Theory]
+    [MemberData(nameof(GetInvalidInputData))]
+    public void Ctor_InvalidInput_ThrowsModelException(string input)
+    {
+        var actual = () => { _ = new Vertex(input); };
+
+        actual.Should().Throw<ModelException>();
+    }
+
+    [Theory]
+    [InlineData("Name1", "name1", true)]
+    [InlineData("Name1", "name2", false)]
+    public void OperatorEquals_CompareNames_ReturnsSuccess(string leftValue, string rightValue, bool expected)
+    {
+        var leftName = new Vertex(leftValue);
+        var rightName = new Vertex(rightValue);
+
+        var actual = leftName == rightName;
+
+        actual.Should().Be(expected);
+    }
+
+    [Fact]
+    public void GetHashCode_NoInput_CreatesHash()
+    {
+        var vertex = new Vertex("Name");
+
+        var actual = () => { _ = vertex.GetHashCode(); };
+
+        actual.Should().NotThrow();
+    }
+
+    [Fact]
+    public void GetHashCode_SameValues_CreatesSameHash()
+    {
+        const string name = "Name";
+        var vertex1 = new Vertex(name.ToLower());
+        var vertex2 = new Vertex(name.ToUpper());
+
+        vertex1.GetHashCode().Should().Be(vertex2.GetHashCode());
+    }
+
+    public static IEnumerable<object[]> GetInvalidInputData() =>
+        new[]
+        {
+            new object[] { null! },
+            new object[] { string.Empty },
+            new object[] { new string('A', Vertex.NameMaxLength + 1) }
+        };
+}
---
+++ src/Core/Vertex.cs
@@ -0,0 +1,72 @@
+﻿using Core.Exceptions;
+
+namespace Core;
+
+/// <summary>
+/// Immutable value object that represents graph's vertex
+/// </summary>
+public sealed class Vertex : IEquatable<Vertex>
+{
+    /// <summary>
+    /// Max. limit of name length
+    /// </summary>
+    public const int NameMaxLength = 255;
+
+    public Vertex(string name)
+    {
+        if (string.IsNullOrWhiteSpace(name))
+        {
+            throw new ModelException("Vertex name cannot be empty");
+        }
+
+        if (name.Length > NameMaxLength)
+        {
+            throw new ModelException($"Vertex name length must be equal or less than {NameMaxLength} chars");
+        }
+
+        Name = name;
+    }
+
+    /// <summary>
+    /// Vertex name
+    /// </summary>
+    public string Name { get; }
+
+    /// <inheritdoc />
+    public override string ToString() => Name;
+
+    public static implicit operator string(Vertex vertex)
+    {
+        return vertex.Name;
+    }
+
+    /// <inheritdoc />
+    public bool Equals(Vertex? other)
+    {
+        if (ReferenceEquals(null, other)) return false;
+        if (ReferenceEquals(this, other)) return true;
+        return string.Equals(Name, other.Name, StringComparison.OrdinalIgnoreCase);
+    }
+
+    /// <inheritdoc />
+    public override bool Equals(object? obj)
+    {
+        return ReferenceEquals(this, obj) || obj is Vertex other && Equals(other);
+    }
+
+    /// <inheritdoc />
+    public override int GetHashCode()
+    {
+        return StringComparer.OrdinalIgnoreCase.GetHashCode(Name);
+    }
+
+    public static bool operator ==(Vertex? left, Vertex? right)
+    {
+        return Equals(left, right);
+    }
+
+    public static bool operator !=(Vertex? left, Vertex? right)
+    {
+        return !Equals(left, right);
+    }
+}
