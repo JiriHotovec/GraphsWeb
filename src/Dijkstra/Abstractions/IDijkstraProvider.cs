516db8eed6ae68162274eb35120cb6c4228481bb
hotovecj <test@example.com>
2023-11-12 12:47:36 +0100

    feat: add interface IDijkstraProvider

---
+++ src/Dijkstra/Abstractions/IDijkstraProvider.cs
@@ -0,0 +1,14 @@
+﻿using Core;
+
+namespace Dijkstra.Abstractions;
+
+/// <summary>
+/// An interface for Dijkstra's provider
+/// </summary>
+public interface IDijkstraProvider
+{
+    Task<IPathResult> GetShortestPathAsync(
+        Vertex source,
+        Vertex destination,
+        CancellationToken cancellationToken = default);
+}
