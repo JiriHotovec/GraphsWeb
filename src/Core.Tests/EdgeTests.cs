f417a6646db50ff7ae896f4cdcb47f4445c55301
hotovecj <test@example.com>
2023-11-12 12:06:46 +0100

    feat: add Edge

---
+++ src/Core.Tests/EdgeTests.cs
@@ -0,0 +1,156 @@
+﻿using Core.Exceptions;
+using FluentAssertions;
+
+namespace Core.Tests;
+
+public class EdgeTests
+{
+    [Theory]
+    [MemberData(nameof(GetValidInputData))]
+    public void Ctor_ValidInput_ReturnsSameValues(Vertex input1, Vertex input2, Edge expected)
+    {
+        var actual = new Edge(input1, input2);
+
+        actual.Should().BeEquivalentTo(expected);
+    }
+
+    [Fact]
+    public void Ctor_InvalidInput_ThrowsModelException()
+    {
+        const string sameName = "SameName";
+
+        var actual = () =>
+        {
+            _ = new Edge(new Vertex(sameName), new Vertex(sameName));
+        };
+
+        actual.Should().Throw<ModelException>();
+    }
+
+    [Theory]
+    [MemberData(nameof(GetEqualityData))]
+    public void OperatorEquals_CompareEdges_ReturnsSuccess(Edge leftValue, Edge rightValue, bool expected)
+    {
+        var actual = leftValue == rightValue;
+
+        actual.Should().Be(expected);
+    }
+
+    [Fact]
+    public void GetHashCode_NoInput_CreatesHash()
+    {
+        var vertex = new Edge(new Vertex("Name1"), new Vertex("Name2"));
+
+        var actual = () =>
+        {
+            _ = vertex.GetHashCode();
+        };
+
+        actual.Should().NotThrow();
+    }
+
+    [Fact]
+    public void GetHashCode_SameValues_CreatesSameHash()
+    {
+        var edge1 = new Edge(new Vertex("Name1"), new Vertex("Name2"));
+        var edge2 = new Edge(new Vertex("Name2"), new Vertex("Name1"));
+
+        edge1.GetHashCode().Should().Be(edge2.GetHashCode());
+    }
+
+    [Fact]
+    public void SwitchVertices_Input_ReturnsEdge()
+    {
+        var edge = new Edge(new Vertex("Name1"), new Vertex("Name2"));
+        var expected = new Edge(new Vertex("Name2"), new Vertex("Name1"));
+
+        var actual = edge.SwitchVertices();
+
+        actual.Source.Should().Be(expected.Source);
+        actual.Destination.Should().Be(expected.Destination);
+    }
+
+    [Theory]
+    [MemberData(nameof(GetHasRelationData))]
+    public void HasRelation_EdgeInput_Returns(Edge left, Edge right, bool expected)
+    {
+        var actual = left.HasRelation(right);
+
+        actual.Should().Be(expected);
+    }
+
+    public static IEnumerable<object[]> GetValidInputData() =>
+        new[]
+        {
+            new object[]
+            {
+                new Vertex("Name1"),
+                new Vertex("Name2"),
+                new Edge(new Vertex("Name1"), new Vertex("Name2"))
+            },
+            new object[]
+            {
+                new Vertex("Name2"),
+                new Vertex("Name1"),
+                new Edge(new Vertex("Name2"), new Vertex("Name1"))
+            }
+        };
+
+    public static IEnumerable<object[]> GetEqualityData() =>
+        new[]
+        {
+            new object[]
+            {
+                new Edge(new Vertex("Name1"), new Vertex("Name2")),
+                new Edge(new Vertex("Name1"), new Vertex("Name2")),
+                true
+            },
+            new object[]
+            {
+                new Edge(new Vertex("Name1"), new Vertex("Name2")),
+                new Edge(new Vertex("Name2"), new Vertex("Name1")),
+                true
+            },
+            new object[]
+            {
+                new Edge(new Vertex("Name1"), new Vertex("Name2")),
+                new Edge(new Vertex("Name1"), new Vertex("Name3")),
+                false
+            },
+            new object[]
+            {
+                new Edge(new Vertex("Name1"), new Vertex("Name2")),
+                new Edge(new Vertex("Name3"), new Vertex("Name2")),
+                false
+            }
+        };
+
+    public static IEnumerable<object[]> GetHasRelationData() =>
+        new[]
+        {
+            new object[]
+            {
+                new Edge(new Vertex("Name1"), new Vertex("Name2")),
+                new Edge(new Vertex("Name2"), new Vertex("Name1")),
+                true
+            },
+            new object[]
+            {
+                new Edge(new Vertex("Name1"), new Vertex("Name2")),
+                new Edge(new Vertex("Name1"), new Vertex("Name2")),
+                true
+            },
+            new object[]
+            {
+                new Edge(new Vertex("Name1"), new Vertex("Name2")),
+                new Edge(new Vertex("Name2"), new Vertex("Name3")),
+                true
+            },
+            new object[]
+            {
+                new Edge(new Vertex("Name1"), new Vertex("Name2")),
+                new Edge(new Vertex("Name3"), new Vertex("Name4")),
+                false
+            },
+        };
+}
---
+++ src/Core/Edge.cs
@@ -0,0 +1,82 @@
+﻿using Core.Abstractions;
+using Core.Exceptions;
+
+namespace Core;
+
+/// <summary>
+/// Edge (A, B) = (B, A)
+/// </summary>
+public class Edge : IEdge, IEquatable<Edge>
+{
+    /// <summary>
+    /// Parametrized ctor
+    /// </summary>
+    /// <param name="source">Vertex source</param>
+    /// <param name="destination">Vertex destination</param>
+    /// <exception cref="ModelException">Custom exception for model validation</exception>
+    /// <exception cref="ArgumentNullException">Parameter cannot be null</exception>
+    public Edge(Vertex source, Vertex destination)
+    {
+        if (source == destination)
+        {
+            throw new ModelException("Edge must have different vertices set");
+        }
+
+        Source = source ?? throw new ArgumentNullException(nameof(source));
+        Destination = destination ?? throw new ArgumentNullException(nameof(destination));
+    }
+
+    /// <inheritdoc />
+    public Vertex Source { get; }
+
+    /// <inheritdoc />
+    public Vertex Destination { get; }
+
+    /// <inheritdoc />
+    public virtual IEdge SwitchVertices() =>
+        new Edge(Destination, Source);
+
+    /// <inheritdoc />
+    public bool HasRelation(IEdge other) =>
+        Source == other.Source ||
+        Source == other.Destination ||
+        Destination == other.Source ||
+        Destination == other.Destination;
+
+    /// <inheritdoc />
+    public override string ToString() => $"({Source}, {Destination})";
+
+    /// <inheritdoc />
+    public bool Equals(Edge? other)
+    {
+        if (ReferenceEquals(null, other)) return false;
+        if (ReferenceEquals(this, other)) return true;
+        return Source.Equals(other.Source) && Destination.Equals(other.Destination) ||
+               Source.Equals(other.Destination) && Destination.Equals(other.Source);
+    }
+
+    /// <inheritdoc />
+    public override bool Equals(object? obj)
+    {
+        if (ReferenceEquals(null, obj)) return false;
+        if (ReferenceEquals(this, obj)) return true;
+        if (obj.GetType() != this.GetType()) return false;
+        return Equals((Edge)obj);
+    }
+
+    /// <inheritdoc />
+    public override int GetHashCode()
+    {
+        return Source.GetHashCode() + Destination.GetHashCode();
+    }
+
+    public static bool operator ==(Edge? left, Edge? right)
+    {
+        return Equals(left, right);
+    }
+
+    public static bool operator !=(Edge? left, Edge? right)
+    {
+        return !Equals(left, right);
+    }
+}
