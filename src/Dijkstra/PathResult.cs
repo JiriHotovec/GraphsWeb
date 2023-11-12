29132a25e50ae3cfe7d7163c24b95a1b95813f2e
hotovecj <test@example.com>
2023-11-12 12:46:17 +0100

    feat: add PathResult

---
+++ src/Dijkstra/PathResult.cs
@@ -0,0 +1,30 @@
+﻿using Core.Abstractions;
+using Dijkstra.Abstractions;
+
+namespace Dijkstra;
+
+/// <summary>
+/// Object result for Dijkstra's provider returned path result
+/// </summary>
+public class PathResult : IPathResult
+{
+    /// <summary>
+    /// Parametrized ctor
+    /// </summary>
+    /// <param name="paths">Returned shortest path collection of edges</param>
+    /// <exception cref="ArgumentNullException"></exception>
+    public PathResult(IEnumerable<IWeightedEdge> paths)
+    {
+        Paths = paths ?? throw new ArgumentNullException(nameof(paths));
+    }
+
+    /// <summary>
+    /// Property returned success state
+    /// </summary>
+    public bool IsSuccess => Paths.Any();
+
+    /// <summary>
+    /// Returned shortest path collection of edges
+    /// </summary>
+    public IEnumerable<IWeightedEdge> Paths { get; }
+}
