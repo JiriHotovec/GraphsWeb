ae52975cbc87e665023da5c37184a9a424a36809
hotovecj <test@example.com>
2023-11-15 20:49:35 +0100

    feat: add GraphExtension

---
+++ src/WebApi/Extensions/GraphExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Core;
+using Core.Abstractions;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+
+namespace WebApi.Extensions;
+
+public static class GraphExtensions
+{
+    public static IServiceCollection AddGraph(this IServiceCollection sc)
+    {
+        ArgumentNullException.ThrowIfNull(sc);
+
+        sc.TryAddScoped<IGraphStorage<WeightedEdge>, GraphJsonFileStorage<WeightedEdge>>();
+
+        return sc;
+    }
+}
