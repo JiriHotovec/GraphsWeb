e2e16b5fb23f8651852be143810725d3230a443f
hotovecj <test@example.com>
2023-11-18 17:50:33 +0100

    feat: add GraphDetail

--- src/BlazorApp/Program.cs
+++ src/BlazorApp/Program.cs
@@ -2,6 +2,8 @@ using BlazorApp;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
+const string webApiBaseAddress = "http://localhost:5089";
+
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
@@ -9,7 +11,7 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 // builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddScoped(sp => new HttpClient
 {
-    BaseAddress = new Uri("http://localhost:5089/api"),
+    BaseAddress = new Uri(webApiBaseAddress),
     Timeout = TimeSpan.FromSeconds(5)
 });
 
